3c624ef108e0ce4b6fb2cd361fbc8f0f0327c8b6
FatihOguz <foguz4999@gmail.com>
2021-08-06 19:51:25 +0300

    Initial commit

---
+++ src/main/java/com/fatih/oguz/Tutorial/TutorialSpringReactMySqlApplication.java
@@ -0,0 +1,13 @@
+package com.fatih.oguz.Tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialSpringReactMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialSpringReactMySqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fatih/oguz/Tutorial/TutorialSpringReactMySqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatih.oguz.Tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialSpringReactMySqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
